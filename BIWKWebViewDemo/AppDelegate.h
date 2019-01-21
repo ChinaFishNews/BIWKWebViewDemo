6b977c6436bb05f101dc2d895a358977ec3d6c41
yuxinwen <yuxinwen@baidu.com>
2019-01-21 18:10:27 +0800

    Initial Commit

---
+++ BIWKWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BIWKWebViewDemo
+//
+//  Created by xinwen on 2019/1/21.
+//  Copyright © 2019年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BIWKWebViewDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BIWKWebViewDemo
+//
+//  Created by xinwen on 2019/1/21.
+//  Copyright © 2019年 baidu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
